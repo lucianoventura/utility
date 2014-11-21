1f634f123e4dda1c46961f9271fc86c79b08bb49
Luciano Ventura Alves <luciano.ventura@gmail.com>
2014-11-21 18:17:31 -0200

    updated

--- backup_directory_files.sh
+++ backup_directory_files.sh
@@ -8,92 +8,114 @@ readonly SUCCESS=0
 readonly FAILURE=1
 
 
+logger_info(){
+    # Function to print messages and variables to STDOUT
+    printf "[$(date +%T)] [INFO] - %s \n" "$@"
+}
+
+
+logger_error(){
+    # Function to print messages and variables to STDERR
+    printf "[$(date +%T)] [ERROR] - %s \n" "$@" 1>&2
+}
+
+
+exit_success(){
+    # Function to print exit success and exit
+    logger_info "$(basename $0): Exiting with success!"
+    exit $SUCCESS
+}
+
+
+exit_failure(){
+    # Function to print exit failure and exit
+    logger_error "$(basename $0): Exiting with failure!"
+    exit $FAILURE
+}
+
+
 show_usage(){
     echo
-    echo "Backup directory files only, do NOT include directories inside."
+    echo
+    echo
+    echo "Backup directory files do NOT include sub-directory"
     echo
     echo "How to use:"
     echo
-    echo "$(basename $0) source_directory files_to_exclude backup_directory backup_name out_log_file err_log_file"
+    echo "$(basename $0) source_directory files_to_exclude backup_directory backup_name"
     echo
-    echo " .tar.gz automagicaly added to the backup_name"
+    echo ".tar.gz automagicaly added to the backup_name"
     echo
-    echo "Exiting..."
+    echo
     echo
 }
 
 
 # Check for missing parameters
-if [ -z "$1" -o -z "$2" -o -z "$3" -o -z "$4" -o -z "$5" -o -z "$6" ]
-then
+if [ -z "$1" -o -z "$2" -o -z "$3" -o -z "$4" ]; then
     show_usage
-    
-    exit $FAILURE
+     
+    exit_failure
 fi
 
 
-  source_dir="$1"       # directory to backup               
-  to_exclude="$2"       # files to exclude from backup      "*.ind|*.pag"   "essbase.sec" "*.shit"
-
-  backup_dir="$3"       # destination backup                /backup/local
-  bakup_name="$4"       # destination file name             dirname_files
-
-out_log_file="$5"       # output log file                   /dev/null for no log
-err_log_file="$6"       # error  log file                   /dev/null for no log
+source_dir="$1"       # directory to backup               
+to_exclude="$2"       # files to exclude from backup      "*.ind|*.pag"   "essbase.sec" "*.shit"
+backup_dir="$3"       # destination backup                /backup/local
+bakup_name="$4"       # destination file name             dirname_files
 
 
-if [ -e "$backup_dir/$bakup_name.tar.gz" ]
-then
-    echo ""                                                                                 | tee    $err_log_file
-    echo "[$(date +%T)] [ERROR] delete $backup_dir/$bakup_name.tar.gz before run!"          | tee -a $err_log_file
-    echo ""                                                                                 | tee -a $err_log_file
-    echo "Exiting..."
-    echo
-    exit $FAILURE
+logger_info "**********************************************"
+logger_info "***   Starting $(basename $0)   ***"
+logger_info "**********************************************"
+
+
+# check if final backup file already exist
+if [ -e "$backup_dir/$bakup_name.tar.gz" ]; then
+    logger_error "delete $backup_dir/$bakup_name.tar.gz before run!"
+     
+    exit_failure
 fi
 
 
 # list everything, except directories and exclude from exclude list
-files_to_bkp=$(find $source_dir -not -type d | egrep -v $to_exclude)
+if ! files_to_bkp=$(find $source_dir -maxdepth 1 ! -type d | egrep -v $to_exclude); then
+    logger_error "can NOT find files inside $source_dir"
+     
+    exit_failure
+fi
 
 
-for item in $files_to_bkp
-do
-    if ! tar --append --file=$backup_dir/$bakup_name.tar $item >$out_log_file 2>$err_log_file   # if fail to execute tar
-    then
-        echo ""                                                                                 | tee -a $err_log_file
-        echo "[$(date +%T)] [ERROR] can NOT append $item into $backup_dir/$bakup_name"          | tee -a $err_log_file
-        echo ""                                                                                 | tee -a $err_log_file
-        echo "[$(date +%T)] [ERROR] check for incomplete file $backup_dir/$bakup_name"          | tee -a $err_log_file
-        echo ""                                                                                 | tee -a $err_log_file
-        exit $FAILURE
+logger_info "Excluding $to_exclude"
+
+
+for item in $files_to_bkp; do
+    if tar --append --file=$backup_dir/$bakup_name.tar $item 2>>/dev/null; then
+        logger_info "added file: $(basename $item)"
+     
+    else
+        logger_error "can NOT append $item into $backup_dir/$bakup_name"
+         
+        logger_error "check for incomplete file $backup_dir/$bakup_name"
+         
+        exit_failure
     fi
 done
 
 
-echo ""                                                                                         | tee -a $out_log_file
-echo "[$(date +%T)] [INFO] Success in append files to $backup_dir/$bakup_name.tar"              | tee -a $out_log_file
-echo ""                                                                                         | tee -a $out_log_file
+if gzip --force $backup_dir/$bakup_name.tar 2>>/dev/null; then
+    logger_info "Success in gzip $backup_dir/$bakup_name.tar"
+     
+    logger_info "Final file is $backup_dir/$bakup_name.tar.gz"
+     
+    exit_success
 
-
-# debug: tar --list --file=$backup_dir/$bakup_name.tar | sort
-# debug: echo
-
-
-if ! gzip --force $backup_dir/$bakup_name.tar                                         >>$out_log_file 2>>$err_log_file
-then
-    echo ""                                                                                     | tee -a $err_log_file
-    echo "[$(date +%T)] [ERROR] can NOT gzip $backup_dir/$bakup_name.tar"                       | tee -a $err_log_file
-    echo ""                                                                                     | tee -a $err_log_file
-    echo "[$(date +%T)] [ERROR] check for incomplete file $backup_dir/$bakup_name.tar"          | tee -a $err_log_file
-    echo ""                                                                                     | tee -a $err_log_file
-    exit $FAILURE
 else
-    echo "[$(date +%T)] [INFO] Success in gzip $backup_dir/$bakup_name.tar"                     | tee -a $out_log_file
-    echo ""                                                                                     | tee -a $out_log_file
-    echo "[$(date +%T)] [INFO] Final file is $backup_dir/$bakup_name.tar.gz"                    | tee -a $out_log_file
-    echo ""                                                                                     | tee -a $out_log_file
-    exit $SUCCESS
+    logger_error "can NOT gzip $backup_dir/$bakup_name.tar"
+     
+    logger_error "check for incomplete file $backup_dir/$bakup_name.tar"
+     
+    exit_failure
 fi
 
 
--- send_email.sh
+++ send_email.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-readonly script_version="1.0.0"
-# utility_send_email.sh
+readonly script_version="1.1.0"
+# send_email.sh
 # Created by:  luciano.ventura@gmail.com   2013_01_01
 
 
@@ -8,33 +8,69 @@ readonly SUCCESS=0                              # generic success
 readonly FAILURE=1                              # generic error
 
 
-if [ -z "$1" -o -z "$2" -o -z "$3" -o -z "$4" -o -z "$5" ]; then
-     
-    echo $(date +%F" "%T) "ERROR: utility_send_email: check parameters"
+logger_info(){
+    # Function to print messages and variables to STDOUT
+    printf "[$(date +%T)] [INFO] - %s \n" "$@"
+}
+
+
+logger_error(){
+    # Function to print messages and variables to STDERR
+    printf "[$(date +%T)] [ERROR] - %s \n" "$@" 1>&2
+}
+
+
+exit_success(){
+    # Function to print exit success and exit
+    logger_info "$(basename $0): Exiting with success!"
+    exit $SUCCESS
+}
+
+
+exit_failure(){
+    # Function to print exit failure and exit
+    logger_error "$(basename $0): Exiting with failure!"
+    exit $FAILURE
+}
+
+
+show_usage(){
     echo
-	exit $FAILURE
+    echo
+    echo
+    echo "send email utility"
+    echo
+    echo "How to use:"
+    echo
+    echo "$(basename $0)  email_list  email_title  email_message"
+    echo
+    echo
+    echo
+}
+
+
+if [ -z "$1" -o -z "$2" -o -z "$3" ]; then
+     
+    show_usage
+     
+    exit_failure
 fi
 
 
-email_list="$1"
-email_title="$2"
+   email_list="$1"
+  email_title="$2"
 email_message="$3"
 
 
-out_log_file="$4"
-err_log_file="$5"
+if echo "$email_message" | mailx -s "$email_title" $email_list; then
+    logger_info "emailed to $email_list"
+     
+    exit_success
 
-
-echo "$email_message" | mailx -s "$email_title" $email_list
-
-
-if [ $? == $SUCCESS ]; then
-
-    echo $(date +%F" "%T) "INFO: emailed to $email_list"                            >> $out_log_file
-    exit $SUCCESS
 else
-    
-    echo $(date +%F" "%T) "ERROR: NOT emailed to $email_list, check email service"  >> $err_log_file
-    exit $FAILURE 
+    logger_error "NOT emailed to $email_list, check email service"
+     
+    exit_failure
 fi
+
 
